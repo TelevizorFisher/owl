ad796cc272ebfb95ca5172bb2a95046e802e538b
TelevizorFisher <65242068+TelevizorFisher@users.noreply.github.com>
2020-05-25 17:35:14 +0300

    а

--- js/main.js
+++ js/main.js
@@ -26,8 +26,8 @@ document.getElementById('id3game__body').hidden = true;
 	document.getElementById("first2").onclick = function () {
 	        var e = document.getElementById("rect");
 	        var a = document.getElementById("menu");
-	            e.style.backgroundImage = 'url("../img/photo.jpg")';
-	            a.style.backgroundImage = 'url("../img/photo.jpg")';
+	            e.style.backgroundImage = 'url("../img/photoй.jpg")';
+	            a.style.backgroundImage = 'url("../img/photoй.jpg")';
 	        }
 	document.getElementById("first3").onclick = function () {
 			 var x = document.getElementById("myColor").value;
@@ -43,21 +43,29 @@ document.getElementById('id3game__body').hidden = true;
 	{
 			document.getElementById("idconteiner4").id = "idconteiner1";
 			document.getElementById("gamecursor4").id = "gamecursor1";
+			document.getElementById("gamecursor34").id = "gamecursor31";
+			document.getElementById("gamecursor24").id = "gamecursor21";
 		}
 	document.getElementById("ikon_2").onclick = function () 
 	{
 			document.getElementById("idconteiner1").id = "idconteiner2"; 
 			document.getElementById("gamecursor1").id = "gamecursor2"; 
+			document.getElementById("gamecursor31").id = "gamecursor32";
+			document.getElementById("gamecursor21").id = "gamecursor22";
 	}
 	document.getElementById("ikon_3").onclick = function () 
 	{
 			document.getElementById("idconteiner2").id = "idconteiner3"; 
 			document.getElementById("gamecursor2").id ="gamecursor3"; 
+			document.getElementById("gamecursor32").id = "gamecursor33";
+			document.getElementById("gamecursor22").id = "gamecursor23";
 	}
 	document.getElementById("ikon_4").onclick = function () 
 	{
 			document.getElementById("idconteiner3").id = "idconteiner4"; 
 			document.getElementById("gamecursor3").id ="gamecursor4"; 
+			document.getElementById("gamecursor33").id = "gamecursor34";
+			document.getElementById("gamecursor23").id = "gamecursor24";
 }
 	document.getElementById("D_1").onclick = function () {
 		document.getElementById('id2game__body').hidden = false;
